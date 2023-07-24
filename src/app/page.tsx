a0fc469adb4796b6b4d8ca83a576e3d314293411
Abelardo Palácios <71283403+AbelardoOk@users.noreply.github.com>
2023-07-24 15:49:35 -0400

    fix: interface inicial totalmente funcional

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,23 @@
+"use client";
+
 import Image from "next/image";
 import { ListIcon } from "../../public/listIcon";
+import { useState } from "react";
 export default function Home() {
+  const [email, setEmail] = useState("");
+  const [validated, setValidated] = useState(false);
+
+  const emailValidation = (e) => {
+    var pattern = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+    var emailValue = e.target.value;
+    setEmail(emailValue);
+    if (email.match(pattern)) {
+      setValidated(true);
+    } else {
+      setValidated(false);
+    }
+  };
+
   return (
     <main className="flex h-screen items-center justify-center bg-neutral-charcoal">
       <div className="flex flex-row items-center rounded-3xl bg-neutral-white shadow-xl">
@@ -24,20 +41,31 @@ export default function Home() {
 
           <form className="flex flex-col gap-6" action="">
             <div className="flex flex-col gap-2">
-              <label className="text-xs font-bold" htmlFor="email">
-                Email adress
-              </label>
+              {validated == true || email == "" ? (
+                <label className="text-xs font-bold" htmlFor="email">
+                  Email adress
+                </label>
+              ) : (
+                <div className="flex flex-row justify-between text-xs font-bold">
+                  <label htmlFor="email">Email adress</label>
+                  <p className="text-primary-tomato">Valid email required</p>
+                </div>
+              )}
+
               <input
-                className="rounded-lg border-2 border-neutral-grey p-4 text-left"
+                className="rounded-lg border-[1px] border-neutral-grey p-4 text-left transition duration-300 invalid:border-primary-tomato invalid:bg-red-100 invalid:text-primary-tomato focus:border-neutral-dark disabled:bg-neutral-white"
                 type="email"
                 name="email"
                 id="email"
+                onChange={emailValidation}
+                value={email}
+                pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
                 placeholder="email@company.com"
               />
             </div>
 
             <button
-              className="rounded-lg bg-neutral-dark py-4 text-center font-bold text-neutral-white"
+              className="rounded-lg bg-neutral-dark py-4 text-center font-bold text-neutral-white transition-all duration-150 hover:bg-gradient-to-r hover:from-pink-500 hover:to-orange-400 hover:shadow-xl hover:shadow-pink-300"
               type="submit"
             >
               Subscribe to monthly newsletter
